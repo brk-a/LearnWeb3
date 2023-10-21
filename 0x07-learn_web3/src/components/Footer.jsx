9833838bf956c6b734a7120bab01ea388c8111df
brk-a <fnjakai@outlook.com>
2023-10-22 02:29:25 +0300

    footer, jsx and css: done

--- 0x07-learn_web3/src/components/Footer.jsx
+++ 0x07-learn_web3/src/components/Footer.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import Image from "next/image"
+import {RiSendPlaneFill} from "react-icons/ri"
+import {
+  TiSocialTwitter,
+  TiSocialLinkedin,
+  TiSocialYoutube,
+  TiSocialFacebook,
+  TiSocialInstagram
+} from "react-icons/ti"
 
+import Style from "./footer.module.css"
+import footerlogo from "../assets/logo-no-bg.png"
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={Style.footer}>
+      <div className={Style.footer__box}>
+        <Image src={footerlogo} alt="logo" width={100} height={100}/>
+      </div>
+      <div className={Style.footer__box}>
+        <div className={Style.footer__input}>
+          <input type="email" placeholder="Email"/>
+          <RiSendPlaneFill/>
+        </div>
+      </div>
+      <div className={Style.footer__box}>
+        <div className={Style.social}>
+            <TiSocialFacebook/>
+            <TiSocialInstagram/>
+            <TiSocialLinkedin/>
+            <TiSocialTwitter/>
+            <TiSocialYoutube/>
+        </div>
+      </div>
+    </div>
   )
 }
 
